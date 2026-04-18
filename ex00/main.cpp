4efb41792f31f0c97cfbb40afde557ab8c4fe024
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-18 08:45:24 +0100

    Editing ex00/a.out ex00/main.cpp,

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,17 +1,17 @@
 
+
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 
 
-
-
-
 template <typename T>
 void    test(T container)
 {
-    typename T::iterator it = container.begin();
+    typename T::iterator it = container.back();
     std::cout << *it << std::endl;
 }
 
@@ -21,4 +21,3 @@ int main()
     std::vector<int> v = {1,2,3};
     test(v);
 }
-
