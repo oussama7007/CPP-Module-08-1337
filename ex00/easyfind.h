10d3abd2ded288dd18fa8e287cc0dec6a16a1d62
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-18 09:52:06 +0100

    Editing ex00/a.out ex00/easyfind.h ex00/main.cpp,

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -8,18 +8,24 @@
 #include <algorithm>
 #include <exception>
 
-template <typename T>
 
-class noumfound : public std::exception
+class NumNotFound : public std::exception
 {
     public:
         const char* what() const throw()
         {
             return ("Value not found in container");
         }
+};
+
+template <typename T>
+
+typename T::iterator    easyfind(T& container, int value)
+{
+    typename T::iterator it = std::find(container.befin(), container.end(), value); // std::find is a standerd algorithm that works m3a ay container cus it use iterators 
+    if(it == container.end())
+        throw NumNotFound();
+    return it;
 }
 
-typename     easyfind(T& container, int value)
-{
-    
-}
+
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,18 +4,50 @@
 #include <vector>
 #include <algorithm>
 
-template <typename T>
-void    test(T container)
-{
-    typename T::iterator it = container.back();
-    std::cout << *it << std::endl;
+// template <typename T>
+// void    test(T container)
+// {
+//     typename T::iterator it = container.back();
+//     std::cout << *it << std::endl;
+// }
+
+
+// int main()
+// {
+//     std::vector<int> v = {1,2,3};
+//     test(v);
+// }
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>   // for std::find
+
+int main() {
+    // 1. Create a vector with some numbers
+    std::vector<int> numbers;
+    numbers.push_back(5);
+    numbers.push_back(12);
+    numbers.push_back(8);
+    numbers.push_back(12);  // duplicate on purpose
+    numbers.push_back(3);
+
+    // 2. Try to find the value 12
+    std::vector<int>::iterator it = std::find(numbers.begin(), numbers.end(), 12);
+
+    // 3. Check the result
+    if (it != numbers.end()) {
+        std::cout << "Value " << *it << " found!" << std::endl;
+        std::cout << "It is at index: " << std::distance(numbers.begin(), it) << std::endl;
+    } else {
+        std::cout << "Value not found." << std::endl;
+    }
+
+    // 4. Try to find a value that doesn't exist
+    it = std::find(numbers.begin(), numbers.end(), 99);
+    if (it == numbers.end()) {
+        std::cout << "99 was not found (correct)." << std::endl;
+    }
+
+    return 0;
 }
-
-
-int main()
-{
-    std::vector<int> v = {1,2,3};
-    test(v);
-}
-
-
