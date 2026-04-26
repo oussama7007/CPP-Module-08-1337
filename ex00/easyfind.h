82911b990e9d8619c23180f75b6a2796d2b818ed
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-26 17:04:46 +0100

    Editing ex00/easyfind.h,

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -11,36 +11,36 @@
 
 
 
-// Custom exception class (optional but nice for evaluation)
-class NotFoundException : public std::exception {
-public:
-    const char* what() const throw() {
-        return "Value not found in container";
-    }
-};
+// // Custom exception class (optional but nice for evaluation)
+// class NotFoundException : public std::exception {
+// public:
+//     const char* what() const throw() {
+//         return "Value not found in container";
+//     }
+// };
 
-template <typename T>
-typename T::iterator easyfind(T &container, int value) {
-    // std::find returns container.end() if value not found
-    typename T::iterator it = std::find(container.begin(), container.end(), value);// std::find is a standerd algorithm that works m3a ay container cus it use iterators
-    if (it == container.end())
-        throw NotFoundException();
-    return it;
-}
+// template <typename T>
+// typename T::iterator easyfind(T &container, int value) {
+//     // std::find returns container.end() if value not found
+//     typename T::iterator it = std::find(container.begin(), container.end(), value);// std::find is a standerd algorithm that works m3a ay container cus it use iterators
+//     if (it == container.end())
+//         throw NotFoundException();
+//     return it;
+// }
 
 
 
 //or 
-// template <typename T>
-// typename T::iterator easyfind(T &container, int value) {
-//     return std::find(container.begin(), container.end(), value); o mn be3d dik lfunction li 3iytat l easyfind ghadi dir 
-                                                                    //  std::vector<int>::iterator it = easyfind(vec, 42);
-                                                                    //     if (it != vec.end()) {
-                                                                    //                     // found
-                                                                    //         } else {
-                                                                    //         // not found
-                                                                    //             }
-// }
+template <typename T>
+typename T::iterator easyfind(T &container, int value) {
+    return std::find(container.begin(), container.end(), value); //o mn be3d dik lfunction li 3iytat l easyfind ghadi dir 
+                                                                     std::vector<int>::iterator it = easyfind(vec, 42);
+                                                                        if (it != vec.end()) {
+                                                                                        // found
+                                                                            } else {
+                                                                            // not found
+                                                                                }
+}
 
 
 
