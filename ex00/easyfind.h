ce63c4c977bacced13e9e33361f9f6aacf14b518
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-26 17:29:57 +0100

    Editing ex00/easyfind.h,

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -12,21 +12,17 @@
 
 
 
-// class NotFoundException : public std::exception {
-// public:
-//     const char* what() const throw() {
-//         return "Value not found in container";
-//     }
-// };
 
-// template <typename T>
-// typename T::iterator easyfind(T &container, int value) {
-    
-//     typename T::iterator it = std::find(container.begin(), container.end(), value);// std::find is a standerd algorithm that works m3a ay container cus it use iterators
-//     if (it == container.end())
-//         throw NotFoundException();
-//     return it;
-// }
+
+class   except : public std::exception
+{
+    public:
+        const char* what() const throw()
+        {
+            return ("Value not found in the container");
+        }
+}
+
 
 
 
