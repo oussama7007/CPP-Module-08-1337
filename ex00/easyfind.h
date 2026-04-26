0c1a75974a5c9d85a875f12493b8271dd07ddf0b
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-26 18:57:34 +0100

    Editing ex00/easyfind.h,

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -1,8 +1,8 @@
 
 
 
-#ifndef "EASYFIND_H"
-#define "EASYFIND_H"
+#ifndef EASYFIND_H
+#define EASYFIND_H
 
 
 
@@ -25,7 +25,7 @@ template <typename T>
 
 typename T::iterator easyfind(T &container, int value)
 {
-    typename T::iterator it = std::find(container.begin(), container.end(), value);;
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
     if(it == container.end())
         throw except();
     return it;
